d2137666ce695f77535fb54354e6acc3550d74dc
WilliamGR-dev <wgirardreydet@gmail.com>
2022-05-09 16:50:10 +0200

    Add Routes, Places Google Api and detecting falling

--- src/components/screens/MapScreen/MapScreen.controller.js
+++ src/components/screens/MapScreen/MapScreen.controller.js
@@ -4,39 +4,120 @@ import authService from '../../../services/authService';
 import alertHelper from '../../../helpers/alertHelper';
 import {useSelector} from 'react-redux';
 import {refreshUserProfileData} from '../../../helpers/reduxHelper';
-
+import Geolocation from 'react-native-geolocation-service';
+import {checkAndRequestMultiplePermissions} from '../../../services/permissionsService';
+import {Platform} from 'react-native';
+import {
+  accelerometer,
+  SensorTypes,
+  setUpdateIntervalForType,
+} from 'react-native-sensors';
+import {PERMISSIONS} from 'react-native-permissions';
+let fallingDate = null;
 const useController = ({}) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isPermissionAuthorized, setIsPermissionAuthorized] = useState(false);
+  const [currentPosition, setCurrentPosition] = useState();
+  const [destination, setDestination] = useState();
+  const [destinationPlaceId, setDestinationPlaceId] = useState(null);
+  const [predictions, setPredictions] = useState([]);
   const userProfile = useSelector(s => s.userProfile);
 
-  useEffect(() => {
-    refreshUserProfileData();
-  }, []);
-
-  const goToFromModal = useCallback(goToScreen => {
-    setIsModalVisible(false);
-    setTimeout(() => {
-      goToScreen();
-    }, 200);
-  }, []);
-
-  const logout = useCallback(async () => {
-    if (!(await alertHelper.confirmLogout())) {
-      return;
+  const requestPermission = useCallback(async () => {
+    const permissions = Platform.select({
+      android: [PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION],
+      ios: [PERMISSIONS.IOS.LOCATION_WHEN_IN_USE],
+    });
+    const isAuthorized = await checkAndRequestMultiplePermissions(permissions);
+    setIsPermissionAuthorized(isAuthorized);
+    if (isAuthorized) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+          setCurrentPosition(position);
+        },
+        error => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+      Geolocation.watchPosition(
+        position => {
+          console.log(position);
+          setCurrentPosition(position);
+        },
+        error => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 15000,
+          maximumAge: 10000,
+          interval: 1000,
+        },
+      );
     }
+  }, []);
+  useEffect(() => {
+    requestPermission();
+    refreshUserProfileData();
+    const sub = checkIfFall();
 
-    setIsModalVisible(false);
-    setTimeout(() => {
-      authService.logout();
-    }, 200);
+    return () => {
+      sub && sub.unsubscribe();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const checkIfFall = useCallback(async () => {
+    setUpdateIntervalForType(SensorTypes.accelerometer, 100);
+    const sub = accelerometer.subscribe(({x, y, z}) => {
+      let isFalling = Math.abs(x) < 1 && Math.abs(y) < 1 && Math.abs(z) < 1;
+      if (isFalling && !fallingDate) {
+        fallingDate = new Date();
+      } else if (!isFalling && fallingDate) {
+        fallingDate = null;
+      } else if (isFalling && fallingDate) {
+        let currentDate = new Date();
+        if (currentDate - fallingDate >= 300) {
+          fallingDate = null;
+          alert('Chute');
+        }
+      }
+    });
+    return sub;
+  }, []);
+
+  const onChangeDestination = useCallback(async destination => {
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCvTx4sUz4AYNZFfYfoaanpdKZ3DbvWeWk
+    &input=${destination}&radius=2000`;
+    console.log(apiUrl);
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+      setPredictions(json.predictions);
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
   }, []);
 
   return {
     isModalVisible,
     setIsModalVisible,
     userProfile,
-    goToFromModal,
-    logout,
+    currentPosition,
+    isPermissionAuthorized,
+    setIsPermissionAuthorized,
+    destination,
+    setDestination,
+    predictions,
+    setPredictions,
+    onChangeDestination,
+    destinationPlaceId,
+    setDestinationPlaceId,
   };
 };
 
--- src/components/screens/MapScreen/MapScreen.js
+++ src/components/screens/MapScreen/MapScreen.js
@@ -1,19 +1,362 @@
 import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
-
-import styles from './MapScreen.styles';
-
+import {View, StyleSheet, TouchableHighlight, Text} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import FormTextInput from '../../inputs/FormTextInput/FormTextInput';
+import MapViewDirections from 'react-native-maps-directions';
 import {useNavigator} from '../../../hooks';
 
 import useController from './MapScreen.controller';
 
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  placeview: {
+    position: 'absolute',
+    top: 10,
+    zIndex: 4,
+  },
+  destination: {
+    marginBottom: '8%',
+    height: '10%',
+  },
+  suggestions: {
+    backgroundColor: 'white',
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+});
+
+const mapStyle = [
+  {
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ebe3cd',
+      },
+    ],
+  },
+  {
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#523735',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#f5f1e6',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#c9b2a6',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#dcd2be',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#ae9e90',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative.neighborhood',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        color: '#ffe605',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dfd2ae',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dfd2ae',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#93817c',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#a5b076',
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#447530',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f1e6',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#fdfcf8',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f8c967',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#e9bc62',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#e98d58',
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#db8555',
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#806b63',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dfd2ae',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#8f7d77',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#ebe3cd',
+      },
+    ],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dfd2ae',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#b9d3c2',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#92998d',
+      },
+    ],
+  },
+];
+
 const MapScreen = props => {
-  const {} = useController(props);
+  const {
+    isPermissionAuthorized,
+    currentPosition,
+    destination,
+    setDestination,
+    onChangeDestination,
+    predictions,
+    setPredictions,
+    destinationPlaceId,
+    setDestinationPlaceId,
+  } = useController(props);
 
   const {} = useNavigator();
 
+  const predictionsView = predictions.map(prediction => (
+    <TouchableHighlight
+      onPress={() => {
+        setDestinationPlaceId(prediction.place_id);
+        setDestination(prediction.description);
+        setPredictions([]);
+      }}
+      key={prediction.place_id}>
+      <View>
+        <Text style={styles.suggestions}>
+          {prediction.structured_formatting.main_text}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  ));
+
   return (
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <View style={styles.placeview}>
+        <FormTextInput
+          style={styles.destination}
+          value={destination}
+          onChangeText={value => {
+            console.log(value);
+            setDestination(value);
+            onChangeDestination(value);
+          }}
+          placeholder="Votre destination"
+        />
+        {predictionsView}
+      </View>
+      {isPermissionAuthorized && currentPosition && (
+        <>
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            region={{
+              latitude: currentPosition.coords.latitude,
+              longitude: currentPosition.coords.longitude,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+            customMapStyle={mapStyle}
+            showsUserLocation={true}>
+            {destinationPlaceId && (
+              <MapViewDirections
+                origin={
+                  currentPosition.coords.latitude.toString() +
+                  ',' +
+                  currentPosition.coords.longitude.toString()
+                }
+                destination={'place_id:' + destinationPlaceId}
+                apikey={'AIzaSyCvTx4sUz4AYNZFfYfoaanpdKZ3DbvWeWk'}
+                strokeWidth={3}
+                strokeColor="hotpink"
+              />
+            )}
+          </MapView>
+          <Text>
+            SPEED : {Math.round(currentPosition.coords.speed) * 3.6} Km/h
+          </Text>
+        </>
+      )}
+    </View>
   );
 };
 
